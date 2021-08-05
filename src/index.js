c538ef3631a4ab88730ebed1e16fe28ec9582bb8
kartik <kartikdogra45@gmail.com>
2021-08-05 11:46:13 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -7,92 +7,165 @@ const yargs = require("yargs");
 require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 
+yargs.command({
+  command : 'add',
+  describe: "Adding a Note",
+  handler : function() {
+      console.log("Adding a New Note!");
+  }
+})
+
+
+
 const getAllProjects = async () => {
   const URL = process.env.URL + `projects`;
-  const projects = await fetch(URL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  }).then((res) => res.json());
-  console.table(projects);
+  try {
+    
+    const projects = await fetch(URL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }).then((res) => res.json());
+    console.table(projects);
+  } catch (error) {
+    console.log(chalk.red("error occurred"));
+  }
 };
+
 
 const getAllActiveTasks = async () => {
   const URL = process.env.URL + `tasks`;
-  const tasks = await fetch(URL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  }).then((res) => res.json());
-  console.table(tasks);
+  try {
+    
+    const tasks = await fetch(URL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }).then((res) => res.json());
+    console.table(tasks);
+  } catch (error) {
+    console.log(chalk.red("error occurred"));
+  }
 };
 
-const getSpecificTask = async (task_id) => {
-  const URL = process.env.URL + `tasks/${task_id}`;
-  const tasks = await fetch(URL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  }).then((res) => res.json());
-  console.table(tasks);
+const getSpecificTask = async (argv) => {
+  const URL = process.env.URL + `tasks/${argv.id}`;
+  try {
+    const tasks = await fetch(URL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }).then((res) => res.json());
+    console.table(tasks);
+    
+  } catch (error) {
+    console.log(chalk.red("error occurred"));
+  }
 };
 //ADD A FUNCTION
+
 const addtask = async (argv) => {
+
   const URL = process.env.URL + `tasks`;
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${TOKEN}`,
   };
   const data = {
-    content: `${argv.content}`,
+    content: `${argv.title}`,
     description: `${argv.description}`,
     due_string: `${argv.time}`,
   };
-
-  await fetch(URL, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(data),
-  })
-    .then((res) => {
-      return res.json();
+  try {
+    
+    await fetch(URL, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
     })
-    .then((json) => {
-      console.log("created succesfully");
-    });
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log("created succesfully");
+      });
+  } catch (error) {
+    console.log(chalk.red("error occurred"));
+  }
 };
 
 
 // CLOSING THE TASK
 const closeSpecificTask = async (argv) => {
+
   const URL = process.env.URL + `tasks/${argv.id}/close`;
   const headers = {
     Authorization: `Bearer ${TOKEN}`,
   };
 
-  await fetch(URL, { method: "POST", headers: headers })
-      console.log("closed task succesfully");
+  try {
+    await fetch(URL, { method: "POST", headers: headers })
+       console.log("closed task succesfully");
+    
+  } catch (error) {
+    console.log(chalk.red("error occurred"));
+  }
+
+
 };
 
 const deleteSpecificTask = async (argv) => {
+
   const URL = process.env.URL + `tasks/${argv.id}`;
   const headers = {
     Authorization: `Bearer ${TOKEN}`,
   };
-
-  await fetch(URL, { method: "DELETE", headers: headers })
-      console.log("task deleted succesfully");
+  try {
+    
+    await fetch(URL, { method: "DELETE", headers: headers })
+    console.log("task deleted succesfully");
+  } catch (error) {
+    console.log(chalk.red("error occurred"));
+  }
 };
 
-
-
-const argv = require("yargs").argv;
 yargs
-  .command("ls", "list all the projects", getAllProjects)
-  .command("ls-active","list all the active tasks in a project",getAllActiveTasks)
-  .command("ls-specific","list specific tasks in a project",getSpecificTask(argv.id))
-  .command("task-add", "add a task", addtask(argv))
-  .command("task-close", "close a specific task", closeSpecificTask(argv))
-  .command("task-delete", "close a specific task", deleteSpecificTask(argv))
+.command({command:"ls",describe:"list all the projects",handler:getAllProjects})
+.command({command:"ls-active",describe:"list all the active tasks",handler:getAllActiveTasks})
+.command({command:"ls-specific",describe:"list details of specific task",builder: {id : {describe: 'task id',demandOption: true,type:'Number'}},handler:getSpecificTask})
+.command({command:"task-add",describe:"add/create a task",builder: {title : {describe: 'title',demandOption: true,type:'String'},description: {describe: 'description',demandOption: true,type:'String'},time:{describe: 'time/schedule',demandOption: true,type:'String'}},handler:addtask})
+.command({command:"task-close",describe:"close a specific task",builder: {id : {describe: 'task id',demandOption: true,type:'Number'}},handler:closeSpecificTask})
+.command({command:"task-delete",describe:"delete a specfic task",builder: {id : {describe: 'task id',demandOption: true,type:'Number'}},handler:deleteSpecificTask}).argv
 
-  .help().argv;
+
+
+/*let command=yargs.argv._[0];
+   
+if(command===`ls`)
+{  
+   getAllProjects();
+}
+else if(command===`ls-active`)
+{
+   getAllActiveTasks();
+}
+else if(command===`ls-specific`)s
+{  
+   getSpecificTask(yargs.argv.id);
+}
+else if(command===`task-add`)
+{
+   addtask(yargs.argv);
+}
+else if(command===`task-close`)
+{
+   closeSpecificTask(yargs.argv);
+}
+else if(command===`task-delete`)
+{
+   deleteSpecificTask(yargs.argv);
+}
+else{
+  console.log(chalk.red("no command found"));
+}
+*/
