fc552525ad4164ded5b81443773d14903a18de5d
kartik <kartikdogra45@gmail.com>
2021-08-05 16:49:22 +0530

    updation in stage2

--- src/addProjectTask.js
+++ src/addProjectTask.js
@@ -4,10 +4,35 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 
+const getAllProjects = async (project_name) => {
+    let URL=process.env.URL + `projects`
+    var k=0;
+    try {
+      
+      const projects =  await fetch(URL, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      }).then((res) => res.json());
+     projects.some(project=>{
+          if(project.name==project_name)
+          {
+              k=project.id;
+              return true;
+          }
+          
+      })
+    return k;
+    }
+     catch (error) {
+      console.log(chalk.red("error in getting projects"));
+    }
+  };
+
 
 
 const addProjectTask = async (argv) => {
-
+    const project_id=await getAllProjects(argv.project_name);
     const URL = "https://api.todoist.com/rest/v1/tasks";
     const headers = {
       "Content-Type": "application/json",
@@ -17,7 +42,7 @@ const addProjectTask = async (argv) => {
       content: `${argv.title}`,
       description: `${argv.description}`,
       due_string: `${argv.time}`,
-      project_id:argv.project_id,
+      project_id:project_id,
 
     };
     try {
@@ -31,7 +56,7 @@ const addProjectTask = async (argv) => {
           return res.json();
         })
         .then((json) => {
-            console.log(chalk.blue(`task created succesfully inside project ${argv.project_id}`));
+            console.log(chalk.blue(`task created succesfully inside project (${argv.project_name})`));
         });
     } catch (error) {
       console.log(chalk.red(error));
--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ yargs
 .command({command:"task-delete",describe:"delete a specfic task",builder: {id : {describe: 'task id',demandOption: true,type:'Number'}},handler:deleteSpecificTask})
 .command({command:"ls-projects",describe:"list all the projects",builder: {id : {describe: 'project name',type:'String'}},handler:getAllProjects})
 .command({command:"project-add",describe:"create a projecct",builder: {name : {describe: 'project name',demandOption: true,type:'String'}},handler:createNewProject})
-.command({command:"project-add-task",describe:"create a task inside projectc",builder: {title : {describe: 'title',demandOption: true,type:'String'},description: {describe: 'description',demandOption: true,type:'String'},time:{describe: 'time/schedule',demandOption: true,type:'String'},project_id : {describe: 'project id',demandOption: true,type:'String'}},handler:addProjectTask})
+.command({command:"project-add-task",describe:"create a task inside projectc",builder: {title : {describe: 'title',demandOption: true,type:'String'},description: {describe: 'description',demandOption: true,type:'String'},time:{describe: 'time/schedule',demandOption: true,type:'String'},project_name : {describe: 'project id',demandOption: true,type:'String'}},handler:addProjectTask})
 .argv
